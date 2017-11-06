e4b00171f7fd82d023e321e40fcc3615b4dbfb1d
UsernameToLon <casey.a.laclair@gmail.com>
2017-11-06 18:34:31 -0500

    Updated all areas to same as old project, issues with other project and gradle

--- app/src/main/java/com/memorease/laclair/android/myapplication/MainActivity.java
+++ app/src/main/java/com/memorease/laclair/android/myapplication/MainActivity.java
@@ -1,13 +1,43 @@
 package com.memorease.laclair.android.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    // Sends user to CreateCardActivity.
+    public void sendToCreateCardActivity(View view){
+        Intent intent = new Intent(this, CreateCardActivity.class);
+        startActivity(intent);
+    }
+
+    // Sends user to AllTopicsActivity.
+    public void sendToAllTopicsActivity(View view){
+        Intent intent = new Intent(this, AllTopicsActivity.class);
+        startActivity(intent);
+    }
+
+    // Sends user to TodaysStudyActivity.
+    public void sendToTodaysStudyActivity(View view){
+        Intent intent = new Intent(this, TodaysStudyActivity.class);
+        startActivity(intent);
+    }
+
+    // Sends user to MyScoresActivity.
+    public void sendToMyScoresActivity(View view){
+        Intent intent = new Intent(this, MyScoresActivity.class);
+        startActivity(intent);
     }
 }
+
