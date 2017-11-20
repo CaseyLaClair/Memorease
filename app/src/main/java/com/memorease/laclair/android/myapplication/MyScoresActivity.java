3ed1b9b4ba7f61de428af2e43e41fa14be7c497c
UsernameToLon <casey.a.laclair@gmail.com>
2017-11-20 14:50:47 -0500

    Compelted My scores activity

--- app/src/main/java/com/memorease/laclair/android/myapplication/MyScoresActivity.java
+++ app/src/main/java/com/memorease/laclair/android/myapplication/MyScoresActivity.java
@@ -1,13 +1,66 @@
 package com.memorease.laclair.android.myapplication;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.memorease.laclair.android.myapplication.data.TopicsDbHelper;
+
+import java.util.ArrayList;
 
 public class MyScoresActivity extends AppCompatActivity {
+
+    TopicsDbHelper topicsDbHelper = new TopicsDbHelper(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_scores);
+
+        ArrayList<String> topics = new ArrayList<>(getTopicFromDB());
+
+        ListAdapter listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, topics);
+
+        ListView listView = findViewById(R.id.myScoresListView);
+        listView.setAdapter(listAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                Intent i = new Intent(MyScoresActivity.this, TopicScoreActivity.class);
+                i.putExtra("topic", String.valueOf(adapterView.getItemAtPosition(position)));
+                startActivity(i);
+            }
+        });
+    }
+
+    public ArrayList<String> getTopicFromDB() {
+
+        ArrayList<String> topics = new ArrayList<>();
+        String query = "SELECT topic FROM topics";
+        Cursor cursor = topicsDbHelper.getReadableDatabase().rawQuery(query, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                //If first value found @ cursor.getColumnIndex(etc) is equal to
+                //any value in topics already, dont add. Else add.
+                if (topics.contains(cursor.getString(cursor.getColumnIndex("topic")))) {
+                    break;
+                } else {
+                    topics.add(cursor.getString(cursor.getColumnIndex("topic")));
+                }
+            } while (cursor.moveToNext());
+
+            cursor.close();
+        }
+
+        return topics;
     }
 }
--- app/src/main/java/com/memorease/laclair/android/myapplication/TopicScoreActivity.java
+++ app/src/main/java/com/memorease/laclair/android/myapplication/TopicScoreActivity.java
@@ -2,12 +2,24 @@ package com.memorease.laclair.android.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class TopicScoreActivity extends AppCompatActivity {
+
+    private TextView topicTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.topic_score_activity);
+
+        topicTextView = findViewById(R.id.myScoresTitle);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String topicPassed = extras.getString("topic");
+
+            topicTextView.setText(topicPassed);
+        }
     }
 }
