2e94bbcab9e6be80e26d51826314c38a7cb43d3c
UsernameToLon <casey.a.laclair@gmail.com>
2018-01-11 14:17:18 -0500

    Short updates

---
+++ app/src/main/java/com/memorease/laclair/android/myapplication/CardFlipActivity.java
@@ -0,0 +1,36 @@
+package com.memorease.laclair.android.myapplication;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by casey on 1/5/2018.
+ */
+public class CardFlipActivity extends Activity {
+
+    /**
+     * A fragment representing the front of the card.
+     */
+    public class CardFrontFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_front, container, false);
+        }
+    }
+
+    /**
+     * A fragment representing the back of the card.
+     */
+    public class CardBackFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_back, container, false);
+        }
+    }
+}
