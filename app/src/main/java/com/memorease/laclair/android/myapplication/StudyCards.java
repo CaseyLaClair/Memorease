a81d58c96764bcd6c69c17d2e414d15402ea98f8
UsernameToLon <casey.a.laclair@gmail.com>
2018-01-13 16:15:53 -0500

    Added flip of the card. Still displays relative layout behind it and no text

--- app/src/main/java/com/memorease/laclair/android/myapplication/StudyCards.java
+++ app/src/main/java/com/memorease/laclair/android/myapplication/StudyCards.java
@@ -35,6 +35,7 @@ public class StudyCards extends AppCompatActivity {
     RadioButton incorrect;
     RadioButton correct;
     CheckBox delete;
+    boolean mShowingBack;
     int rightOrWrong;
 
 
@@ -84,7 +85,7 @@ public class StudyCards extends AppCompatActivity {
         }
     }
 
-    public class CardFrontFragment extends Fragment {
+    public static class CardFrontFragment extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
@@ -92,7 +93,7 @@ public class StudyCards extends AppCompatActivity {
         }
     }
 
-    public class CardBackFragment extends Fragment {
+    public static class CardBackFragment extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
@@ -114,6 +115,44 @@ public class StudyCards extends AppCompatActivity {
             qaTextView.setText(question);
         }
 
+        if (mShowingBack) {
+            getFragmentManager().popBackStack();
+            return;
+        }
+
+        // Flip to the back.
+
+        mShowingBack = true;
+
+        // Create and commit a new fragment transaction that adds the fragment for
+        // the back of the card, uses custom animations, and is part of the fragment
+        // manager's back stack.
+
+        getFragmentManager()
+                .beginTransaction()
+
+                // Replace the default fragment animations with animator resources
+                // representing rotations when switching to the back of the card, as
+                // well as animator resources representing rotations when flipping
+                // back to the front (e.g. when the system Back button is pressed).
+                .setCustomAnimations(
+                        R.animator.card_flip_right_in,
+                        R.animator.card_flip_right_out,
+                        R.animator.card_flip_left_in,
+                        R.animator.card_flip_left_out)
+
+                // Replace any fragments currently in the container view with a
+                // fragment representing the next page (indicated by the
+                // just-incremented currentPage variable).
+                .replace(R.id.container, new CardBackFragment())
+
+                // Add this transaction to the back stack, allowing users to press
+                // Back to get to the front of the card.
+                .addToBackStack(null)
+
+                // Commit the transaction.
+                .commit();
+
     }
 
     /**
@@ -125,7 +164,7 @@ public class StudyCards extends AppCompatActivity {
      */
     public void nextCard(View view) throws SQLException {
 
-        if (qaTextView.getText().equals("No Cards Available"))
+        if (qaTextView.getText().equals("No Cards Available")||qaTextView.getText().equals("Nothing On This Side Either"))
             return;
 
         if (delete.isChecked()) {
